7e4cae4b25d4ea7decb33da72cfc2e91c4217d7f
Paul Bottein <paul.bottein@gmail.com>
2019-08-28 22:35:10 +0200

    fix: try to fix intercom loading on android

--- src/web.ts
+++ src/web.ts
@@ -18,38 +18,38 @@ export class IntercomWeb extends WebPlugin implements IntercomPlugin {
     const app_id = config.app_id;
 
     window.intercomSettings = config;
-
-    (async () => {
-      var w = window;
-      var ic = w.Intercom;
-      if (typeof ic === "function") {
-        ic("reattach_activator");
-        ic("update", w.intercomSettings);
-      } else {
-        var d = document;
-        var i = <any>function() {
-          i.c(arguments);
-        };
-        i.q = [];
-        i.c = function(args: any) {
-          i.q.push(args);
-        };
-        w.Intercom = i;
-        var l = function() {
-          var s = d.createElement("script");
-          s.type = "text/javascript";
-          s.async = true;
-          s.src = `https://widget.intercom.io/widget/${app_id}`;
-          var x = d.getElementsByTagName("script")[0];
-          x.parentNode.insertBefore(s, x);
-        };
-        if (w.attachEvent) {
-          w.attachEvent("onload", l);
-        } else {
-          w.addEventListener("load", l, false);
-        }
-      }
-    })();
+    window.Intercom = () => { console.log(app_id)};
+    // (async () => {
+    //   var w = window;
+    //   var ic = w.Intercom;
+    //   if (typeof ic === "function") {
+    //     ic("reattach_activator");
+    //     ic("update", w.intercomSettings);
+    //   } else {
+    //     var d = document;
+    //     var i = <any>function() {
+    //       i.c(arguments);
+    //     };
+    //     i.q = [];
+    //     i.c = function(args: any) {
+    //       i.q.push(args);
+    //     };
+    //     w.Intercom = i;
+    //     var l = function() {
+    //       var s = d.createElement("script");
+    //       s.type = "text/javascript";
+    //       s.async = true;
+    //       s.src = `https://widget.intercom.io/widget/${app_id}`;
+    //       var x = d.getElementsByTagName("script")[0];
+    //       x.parentNode.insertBefore(s, x);
+    //     };
+    //     if (w.attachEvent) {
+    //       w.attachEvent("onload", l);
+    //     } else {
+    //       w.addEventListener("load", l, false);
+    //     }
+    //   }
+    // })();
   }
 
   registerIdentifiedUser(identity: IntercomIdentity): Promise<void> {
