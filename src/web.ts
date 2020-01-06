a85afc394e27b5db89a74d2df0b94eb808ced506
Paul Bottein <paul.bottein@gmail.com>
2020-01-06 13:58:48 +0100

    Revert "feat(upscope): add updscope"
    This reverts commit 0a710aff609760a8e4da975ab2b412432604df15.

--- src/web.ts
+++ src/web.ts
@@ -51,28 +51,6 @@ export class IntercomWeb extends WebPlugin implements IntercomPlugin {
         }
       }
     })();
-
-    (function(w, u, d) {
-      var i = <any>function() {
-        i.c(arguments);
-      };
-      i.q = [];
-      i.c = function(args: any) {
-        i.q.push(args);
-      };
-      var l = function() {
-        var s = d.createElement("script");
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://code.upscope.io/AqqvdN1yfr.js";
-        var x = d.getElementsByTagName("script")[0];
-        x.parentNode.insertBefore(s, x);
-      };
-      if (typeof u !== "function") {
-        w.Upscope = i;
-        l();
-      }
-    })(window, window.Upscope, document);
   }
 
   registerIdentifiedUser(identity: IntercomIdentity): Promise<void> {
