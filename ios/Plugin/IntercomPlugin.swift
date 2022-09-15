84750b486e47ca768376c9d6f3c2fb1dd62a8909
Oupsla <nicolas_delperdange@hotmail.com>
2022-09-15 10:00:27 +0200

    feat(sdk)!: change deprecated ios functions + prepare for sdk v13
    BREAKING CHANGE: registerIdentifiedUser renamed to loginUser
    BREAKING CHANGE: registerUnidentifiedUser renamed to loginUnidentifiedUser

--- ios/Plugin/IntercomPlugin.swift
+++ ios/Plugin/IntercomPlugin.swift
@@ -4,7 +4,7 @@ import Intercom
 
 
 enum PluginError: Error {
-    case initialize 
+    case initialize
 }
 
 /**
@@ -14,7 +14,7 @@ enum PluginError: Error {
 @objc(IntercomPlugin)
 public class IntercomPlugin: CAPPlugin {
     
-
+    
     @objc func initialize(_ call: CAPPluginCall) {
         do {
             let apiKey = getConfigValue("ios_apiKey") as? String ?? "ADD_IN_CAPACITOR_CONFIG_JSON"
@@ -23,130 +23,158 @@ public class IntercomPlugin: CAPPlugin {
             NotificationCenter.default.addObserver(self, selector: #selector(self.didRegisterWithToken(notification:)), name: .capacitorDidRegisterForRemoteNotifications, object: nil)
             call.resolve()
         } catch {
-            call.reject("Could not initialize the Intercom plugin");
+            call.reject("[Intercom SDK] Could not initialize the Intercom plugin");
         }
     }
-
+    
     @objc func didRegisterWithToken(notification: NSNotification) {
         guard let deviceToken = notification.object as? Data else {
             return
         }
-        Intercom.setDeviceToken(deviceToken)
+        
+        do {
+            Intercom.setDeviceToken(deviceToken) { error in
+                print("[Intercom SDK] Error while setting device token")
+                print(error ?? "Unknwown error")
+            }
+        }
     }
-
+    
     @objc func updateUser(_ call: CAPPluginCall) {
         let email = call.getString("email")
         let phone = call.getString("phone")
         let name = call.getString("name")
         let language = call.getString("language")
-
+        
         let userAttributes = ICMUserAttributes()
         userAttributes.email = email
         userAttributes.phone = phone
         userAttributes.name = name
         userAttributes.languageOverride = language
-
-        Intercom.updateUser(userAttributes)
-        call.resolve()
+        
+        Intercom.updateUser(with: userAttributes) { result in
+            switch result {
+            case .success:
+                call.resolve()
+            case .failure(let error):
+                call.reject("[Intercom SDK] Error while updating user: ", error.localizedDescription)
+            }
+        }
     }
-
+    
     @objc func setCustomAttributes(_ call: CAPPluginCall) {
         let attributes = call.getObject("attributes")
-
+        
         let userAttributes = ICMUserAttributes()
         userAttributes.customAttributes = attributes
-        Intercom.updateUser(userAttributes)
-
-        call.resolve()
+        
+        Intercom.updateUser(with: userAttributes) { result in
+            switch result {
+            case .success:
+                call.resolve()
+            case .failure(let error):
+                call.reject("[Intercom SDK] Error while setting custom attribute(s): ", error.localizedDescription)
+            }
+        }
     }
-
-    @objc func registerIdentifiedUser(_ call: CAPPluginCall) {
+    
+    @objc func loginUser(_ call: CAPPluginCall) {
         let userId = call.getString("userId")
         let email = call.getString("email")
         let hmac = call.getString("userHash")
-
+        
+        if(userId == nil && email == nil) {
+            call.reject("[Intercom SDK] You must supply an email, userId or both")
+            return
+        }
+        
         if hmac != nil {
             Intercom.setUserHash(hmac!)
         }
-
-        if userId != nil && email != nil {
-            Intercom.registerUser(withUserId: userId!, email: email!)
-        } else if userId != nil {
-            Intercom.registerUser(withUserId: userId!)
-        } else if email != nil {
-            Intercom.registerUser(withEmail: email!)
-        } else {
-            call.reject("No user registered. You must supply an email, userId or both")
+        
+        let attributes = ICMUserAttributes()
+        attributes.userId = userId
+        attributes.email = email
+        
+        Intercom.loginUser(with: attributes) { result in
+            switch result {
+            case .success:
+                call.resolve()
+            case .failure(let error):
+                call.reject("[Intercom SDK] Error while login user: ", error.localizedDescription);
+            }
         }
-
-        call.resolve()
     }
-
-    @objc func registerUnidentifiedUser(_ call: CAPPluginCall) {
-        Intercom.registerUnidentifiedUser()
-        call.resolve()
+    
+    @objc func loginUnidentifiedUser(_ call: CAPPluginCall) {
+        Intercom.loginUnidentifiedUser { result in
+            switch result {
+            case .success:
+                call.resolve()
+            case .failure(let error):
+                call.reject("[Intercom SDK] Error while login unidentified user: ", error.localizedDescription)
+            }
+        }
     }
-
+    
     @objc func logout(_ call: CAPPluginCall) {
         Intercom.logout()
         call.resolve()
     }
-
+    
     @objc func logEvent(_ call: CAPPluginCall) {
         let eventName = call.getString("name")
         let metaData = call.getObject("data")
-
+        
         if eventName != nil && metaData != nil {
             Intercom.logEvent(withName: eventName!, metaData: metaData!)
-
+            
         } else if eventName != nil {
             Intercom.logEvent(withName: eventName!)
         }
-
+        
         call.resolve()
     }
-
+    
     @objc func displayMessenger(_ call: CAPPluginCall) {
         Intercom.presentMessenger()
         call.resolve()
     }
-
+    
     @objc func displayMessageComposer(_ call: CAPPluginCall) {
         guard let initialMessage = call.getString("content") else {
-            call.reject("Enter an initial message")
+            call.reject("[Intercom SDK] You must provide an initial message")
             return
         }
         Intercom.presentMessageComposer(initialMessage)
         call.resolve()
     }
-
+    
     @objc func displayHelpCenter(_ call: CAPPluginCall) {
         Intercom.presentHelpCenter()
         call.resolve()
     }
-
+    
     @objc func displayArticle(_ call: CAPPluginCall) {
-        let articleId = call.getString("id")
-
-        if articleId != nil {
-            Intercom.presentArticle(articleId!)
-            call.resolve()
-        } else {
-            call.reject("No article id. You must supply an articleId to display it")
+        guard let articleId = call.getString("id") else {
+            call.reject("[Intercom SDK] You must supply an articleId to display it")
+            return
         }
-
+        
+        Intercom.presentArticle(articleId)
+        call.resolve()
     }
-
+    
     @objc func hideMessenger(_ call: CAPPluginCall) {
         Intercom.hide()
         call.resolve()
     }
-
+    
     @objc func displayLauncher(_ call: CAPPluginCall) {
         Intercom.setLauncherVisible(true)
         call.resolve()
     }
-
+    
     @objc func hideLauncher(_ call: CAPPluginCall) {
         Intercom.setLauncherVisible(false)
         call.resolve()
